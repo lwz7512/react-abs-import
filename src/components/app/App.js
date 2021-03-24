fd9487fb8f3f0a3a89e84bc51c7c224ce9e63315
Wenzhi Li <wenzhili@wli-mb.verbinteractive.com>
2021-03-24 13:02:49 -0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+
+import 'style/App.css'
+import Header from 'components/common/Header'
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+    </div>
+  );
+}
+
+export default App;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+import logo from 'logo.svg';
+export default function () {
+    return (
+    <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +15,5 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    )
 }
-
-export default App;
